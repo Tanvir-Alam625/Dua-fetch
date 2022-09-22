0e87e090356093ae8cab069928552bc6ddcb97c0
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-09-23 01:46:41 +0600

    done

--- src/components/Home/Dua.js
+++ src/components/Home/Dua.js
@@ -1,25 +1,29 @@
 import React from "react"
 
-const Dua = ()=>{
+const Dua = ({d})=>{
     return(
-        <div className="card card-compact p-4 border w-full bg-base-100 shadow-xl">
-           <h2 className=" text-xl my-4">Dua Title </h2>
+        <div id={d?.dua_id} className="card card-compact p-4 mb-4 border w-full bg-base-100 shadow-xl">
+           <h2 className=" text-xl my-4">{d?.dua_name_en} </h2>
            <div className="p-2 border-y">
                 <p>Say ten times -</p>
                 <div className="arabic-text flex justify-end">
-                    <p className="my-2 text-2xl"> الإعلان</p>
+                    <p className="my-2 text-2xl">{d?.dua_arabic}</p>
                 </div>
-                <p className='my-4'>dua english dua english  dua english dua english  dua english </p>
+                <p className='my-4'>{d?.bottom_bn} </p>
+                <p className='my-4'>{d?.bottom_en} </p>
                 <a href="" className="text-green-500 font-semibold"> Reference</a>
-                <p>Bukhari No: </p>
+                <p>{d?.refference_en}</p>
             </div>
             <div className="dua-footer my-3">
-            <audio controls>
-                {/* <source src="horse.ogg" type="audio/ogg"> */}
-                Your browser does not support the audio tag.
-            </audio>
+                {
+                    d?.audio && 
+                    <audio controls>
+                    <source src={d?.audio} type="audio/ogg" />
+                </audio>
+                }
+          
             <div className='dua-icon'>
-                
+
             </div>
             </div>
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,41 @@
-import React,{useState} from "react"
+import React,{useState,useEffect} from "react"
 import Dua from "./Dua"
-import Spinner from '../Shared/Spinner'
+import Spinner from '../Shared/Spinner';
+import "../../App.css"
+import Category from "../Category/Category"
+import Setting from "../Setting/Setting"
+
 const Home = ()=>{
-    const [spinner, setSpinner]=useState(true);
-    
-    if(spinner){
-        return <Spinner />
-    }
+    const [spinner, setSpinner]=useState(null);
+    const [duaId,setDuaId]=useState(1);
+    const [dua,setDua]=useState(null);
+    const [category,setCategory]=useState(null);
+   
+    useEffect(()=>{
+     
+        fetch(`https://dua-backend.herokuapp.com/dua-main/dua/${duaId}`)
+        .then(res=>res.json())
+        .then(data=> setDua(data?.result))
+  
+    },[duaId])
+    const getCategory = () =>{
+        fetch(`https://dua-backend.herokuapp.com/dua-main/category`).then(res=>res.json()).then(data=> setCategory(data?.result))
+      } 
+    getCategory();
+    const handelDua=(id)=>{
+        setDuaId(id);
+      }
     return(
-        <main className="main-content min-h-[100vh]  overflow-y-scroll">
-            <Dua />
+        <main className="app min-h-[100vh] mt-2 ">
+            {
+            category && <Category handelDua={handelDua}  category={category}/>
+            }
+           <div className="w-full h-[100vh] overflow-y-scroll mt-4">
+                 {
+                    dua?.map(d=>   <Dua  d={d}/>)
+                }
+           </div>
+            <Setting />
         </main>
     );
 }
